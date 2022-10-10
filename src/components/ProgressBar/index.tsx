a45ff4747232f83192dd009879a678c95e492406
doheek2 <doheek2@naver.com>
2022-10-10 22:17:13 +0900

    fix: 설문 진행 상황 기능 추가

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,17 +1,39 @@
-import styles from './progressBar.module.scss'
+import { Fragment } from 'react'
 import cx from 'classnames'
 
+import styles from './progressBar.module.scss'
+
 interface IProps {
-  num: number
+  questionCount: number
+  progressNum: number
 }
-const ProgressBar = ({ num }: IProps) => {
+
+const ProgressBar = ({ questionCount, progressNum }: IProps) => {
+  const arr = Array(questionCount - 1).fill(0)
+
   return (
     <div className={styles.container}>
       <div className={cx(styles.dot, styles.completionDot)} />
-      <div className={cx(styles.line, styles.completionLine)} style={{ width: `calc(100% / ${num})` }} />
-      <div className={cx(styles.dot, styles.completionDot)} />
-      <div className={cx(styles.line, styles.progressLine)} style={{ width: `calc(100% / ${num})` }} />
-      <div className={cx(styles.dot, styles.progressDot)} />
+      {arr.map((_, i) => {
+        const key = `progress${i}`
+        if (i + 1 <= progressNum) {
+          return (
+            <Fragment key={key}>
+              <div
+                className={cx(styles.line, styles.completionLine)}
+                style={{ width: `calc(100% / ${questionCount})` }}
+              />
+              <div className={cx(styles.dot, styles.completionDot)} />
+            </Fragment>
+          )
+        }
+        return (
+          <Fragment key={key}>
+            <div className={cx(styles.line, styles.progressLine)} style={{ width: `calc(100% / ${questionCount})` }} />
+            <div className={cx(styles.dot, styles.progressDot)} />
+          </Fragment>
+        )
+      })}
     </div>
   )
 }
