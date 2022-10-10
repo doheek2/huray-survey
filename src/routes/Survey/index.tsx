5e18ed2f37e0068b513816461704120250262db6
doheek2 <doheek2@naver.com>
2022-10-10 22:20:13 +0900

    feat: 설문 단답형/복수형 선택 기능 추가, 버튼 클릭했을 때 디자인 추가, footer 버튼 로직 수정, [다음] 버튼 disable 추가

--- src/routes/Survey/index.tsx
+++ src/routes/Survey/index.tsx
@@ -1,64 +1,82 @@
 import { useLocation } from 'react-router-dom'
-import { useState } from 'react'
+import { MouseEvent, useEffect } from 'react'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import cx from 'classnames'
 
 import Header from 'components/Header'
 import ProgressBar from 'components/ProgressBar'
+import FooterBtn from 'components/FooterBtn'
 
-import dataList from 'data/questions.json'
-import answerList from 'data/answers.json'
+import { isAnsClickedState, questionNumState, titleState } from 'store/atom'
+import questionData from 'data/questions.json'
+import answerData from 'data/answers.json'
 
-import prevBtn from 'images/icon-back-grey.png'
-import nextPrimaryBtn from 'images/icon-back-primary.png'
 import styles from './survey.module.scss'
 
-// TODO :: 단답형/복수형 선택 기능, 버튼 Clicked 디자인, footer 버튼 로직 수정, next disable 로직 및 디자인
 const Survey = () => {
   const location = useLocation()
-  const state = location.state as { title: string; questions: Array<number> }
-  const { title, questions } = state
-  const questionsLength = questions.length
 
-  const [questionNum, setQuestionNum] = useState(0)
+  const state = location.state as { questions: Array<number> }
+  const { questions } = state
+
+  const title = useRecoilValue(titleState)
+  const questionNum = useRecoilValue(questionNumState)
+  const [isAnswersClicked, setIsAnswersClicked] = useRecoilState(isAnsClickedState)
+
   const currentNum = questions[questionNum]
+  const currenData = questionData.questions[currentNum]
 
-  const prevBtnClickHandler = () => setQuestionNum((prev) => prev - 1)
-  const nextBtnClickHandler = () => setQuestionNum((prev) => prev + 1)
+  const answerBtnClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
+    const index = Number(e.currentTarget.value)
+    let arr = []
+
+    if (isAnswersClicked[index] || currenData.mode === 0) {
+      arr = Array(currenData.answers.length).fill(false)
+    } else {
+      arr = [...isAnswersClicked]
+    }
+
+    if (!isAnswersClicked[index]) arr[index] = true
+    setIsAnswersClicked(arr)
+  }
+
+  useEffect(() => {
+    const arr = Array(currenData.answers.length).fill(false)
+    setIsAnswersClicked(arr)
+  }, [currenData.answers.length, setIsAnswersClicked])
 
   return (
     <>
       <Header text='' />
       <main>
         <article className={styles.header}>
-          <ProgressBar num={questionsLength} />
+          <ProgressBar questionCount={questions.length} progressNum={questionNum} />
           <p>{title}</p>
         </article>
         <article className={styles.main}>
           <p className={styles.questionCount}>
-            <span>{questionNum + 1}</span>/ {questionsLength}
+            <span>{questionNum + 1}</span>/ {questions.length}
           </p>
-          <p className={styles.question}>{dataList.questions[currentNum].title}</p>
+          <p className={styles.question}>{questionData.questions[currentNum].title}</p>
           <div className={styles.answersBtnContainer}>
-            {dataList.questions[currentNum].answers.map((answerNum, i) => {
+            {currenData.answers.map((answerNum, i) => {
               const key = `answer${i}`
               return (
-                <button type='button' key={key}>
-                  {answerList.answers[answerNum]}
+                <button
+                  type='button'
+                  key={key}
+                  value={i}
+                  className={cx(isAnswersClicked[i] && styles.clicked)}
+                  onClick={answerBtnClickHandler}
+                >
+                  {answerData.answers[answerNum]}
                 </button>
               )
             })}
           </div>
         </article>
       </main>
-      <footer className={styles.footer}>
-        <button type='button' className={styles.prevBtn} onClick={prevBtnClickHandler}>
-          <img src={prevBtn} alt='prev button' />
-          이전
-        </button>
-        <button type='button' className={styles.nextBtn} onClick={nextBtnClickHandler}>
-          다음
-          <img src={nextPrimaryBtn} alt='next button' />
-        </button>
-      </footer>
+      <FooterBtn type='proceeding' questionsLength={questions.length} />
     </>
   )
 }
